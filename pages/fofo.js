b5e0c3b421e4d3f1929c5096700225786041d481
Crooks and Company <95453777+crooks-company@users.noreply.github.com>
2022-05-03 18:33:49 -0600

    initial structure

--- pages/fofo.js
+++ pages/fofo.js
@@ -1,15 +1,15 @@
 import Head from "next/head";
 
-export default function Home() {
+export default function fofo() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>fofo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
         <h1>hola</h1>
-        <a href="/fofo">ir a fofo</a>
+        <a href="/home">ir a home</a>
       </div>
     </div>
   );
